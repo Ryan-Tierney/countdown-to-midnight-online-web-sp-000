05c8eec138dadffd83994ea80d460f2d515226e9
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-05-11 15:53:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,7 +12,7 @@ def countdown_with_sleep(integer)
     while integer > 0 
     puts "#{integer} SECOND(S)!"
     integer -= 1 
-    sleep(.5)
+    sleep(10)
     end 
     return "HAPPY NEW YEAR!" 
   end 
